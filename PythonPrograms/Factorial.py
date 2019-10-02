223d6c7269aaea066efb3cc0646eb114ed63a06c
Saravanakumar Muthusamy <msaravanan@live.com>
2019-10-02 00:18:53 -0700

    space removed

--- PythonPrograms/Factorial.py
+++ PythonPrograms/Factorial.py
@@ -1,13 +1,10 @@
-inputVal = int(input("Enter a value which you want to find factorial:"))
- 
+inputVal = int(input("Enter a value which you want to find factorial:")) 
 factValue = 1
-
 if(inputVal==0):
     factValue=0
 else:
     for fact in range(1,inputVal + 1):
-            factValue = factValue * fact
-    
+            factValue = factValue * fact    
 print("Factorial value is :" + str(factValue))
 
     
